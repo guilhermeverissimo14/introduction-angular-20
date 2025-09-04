09dcac587dd001966aa271a83f8519aaacc07940
guilhermeverissimo14 <guilhermeverissimo14@gmail.com>
2025-09-03 22:12:54 -0300

    style: add style

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component} from '@angular/core';
 
-import { RouterLink, RouterOutlet } from '@angular/router';
+import {RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
-  imports: [RouterOutlet, RouterLink]
+  imports: [RouterOutlet]
 })
 export class App{
 
--- src/app/features/list/list.ts
+++ src/app/features/list/list.ts
@@ -6,10 +6,17 @@ import { UsersList } from "./components/users-list/users-list";
 import { SearchInput } from "./components/search-input/search-input";
 import { User } from "../../shared/interfaces/user";
 import { take } from "rxjs";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-list',
   template: `
+
+        <div>
+          <!-- Com o Router Link você navega entre as rotas da aplicação sem recarregar a página. Ou seja uma navegação que acontece dentro do index.html -->
+          <a routerLink="/create">Criar Usuário</a>  
+        </div>
+
           <!-- Esse [(search)] é um banana in de box, que está sendo usado assim por causa do model 
           que criamos no SearchInput.ts, ele é mesma coisa de um input e output juntos, ou seja,
           ele emite o valor que está dentro do model e também recebe um valor para colocar dentro do model. -->
@@ -22,7 +29,7 @@ import { take } from "rxjs";
         }
     `,  //template significa que o html vai ficar nesse aquivo, templateUrl significa que o html vai ficar em outro arquivo ex: templateUrl: './list.html'
   styles: '',
-  imports: [UsersList, SearchInput],  //styles significa que o css vai ficar nesse aquivo, styleUrl significa que o css vai ficar em outro arquivo ex: styleUrl: './list.scss'
+  imports: [UsersList, SearchInput, RouterLink],  //styles significa que o css vai ficar nesse aquivo, styleUrl significa que o css vai ficar em outro arquivo ex: styleUrl: './list.scss'
 })
 export class List {
   userService = inject(UserService);
@@ -49,30 +56,30 @@ export class List {
     this.getUsers();
   })
 
-  
+
   ngOnInit() {
     this.getUsers();
   }
-  
-  
+
+
   remove({ id }: User) {
     this.userService.delete(id).subscribe(() => {
       this.users.update(users => users.filter(u => u.id !== id));
     });
-    
+
   }
 
   private getUsers() {
 
     // Se tem alguma coisa no search que vem do valor digitado o computed reagi e filtra.
     this.userService.getAll(this.search())
-    .pipe(
-      takeUntilDestroyed(this.destroyRef),
-      take(1)
-    )
-    .subscribe(users => {
-      this.users.set(users);
-      this.isLoading.set(false);
-    });
+      .pipe(
+        takeUntilDestroyed(this.destroyRef),
+        take(1)
+      )
+      .subscribe(users => {
+        this.users.set(users);
+        this.isLoading.set(false);
+      });
   }
 }
